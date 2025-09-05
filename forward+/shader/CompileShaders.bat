a2824cdadd69849a6a964a3ade8ed18e36119df8
wx321321321 <1311945742@qq.com>
2025-09-05 16:37:03 +0800

    Delete forward+ directory

--- forward+/shader/CompileShaders.bat
+++
@@ -1,6 +0,0 @@
-glslc forwardplus.vert -o forwardplus_vert.spv
-glslc forwardplus.frag -o forwardplus_frag.spv
-glslc light_culling.comp -o light_culling_comp.spv 
-glslc depth.vert -o depth_vert.spv
-glslc depth.frag -o depth_frag.spv
-
--- forward+/shader/depth.frag
+++
@@ -1,5 +0,0 @@
-#version 450
-#extension GL_ARB_separate_shader_objects : enable
-
-// 空片段着色器：仅声明main函数，无任何操作
-void main() {}
